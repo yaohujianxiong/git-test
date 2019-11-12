996e0f0f1092b48a8b8af7ae2a23940ff0230038
aoxin <2245006465@qq.com>
2019-11-12 18:45:36 +0800

    添加内容1

--- git-test-project/src/com/itcast/git/FirstGit.java
+++ git-test-project/src/com/itcast/git/FirstGit.java
@@ -3,5 +3,7 @@ package com.itcast.git;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        //添加内容，来测试
+        System.out.println("come on !!");
     }
 }
